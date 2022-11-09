a8222e8b34435172e07f13c977dfb25a0fc6e1a1
Mateusz <mateusz1bukowski@gmail.com>
2022-11-09 08:10:46 +0100

    comment cart errors

--- src/main/java/models/Cart.java
+++ src/main/java/models/Cart.java
@@ -7,22 +7,21 @@ import java.util.List;
 public class Cart {
     private List<Product> products;
     BigDecimal totalOrderCost;
-
-    public Cart() {
-        products = new ArrayList<>();
-    }
-
-    public void addProduct(Product product) {
-        totalOrderCost = getTotalOrderCost();
-        if ((products.size()) == 0) {
-            products.add(product);
-            totalOrderCost = getTotalOrderCost();
-        } else {
-            addProductToCart();
-            totalOrderCost = getTotalOrderCost();
-        }
+}
+//    public Cart() {
+//        products = new ArrayList<>();
+//    }
+//
+//    public void addProduct(Product product) {
+//        totalOrderCost = getTotalOrderCost();
+//        if ((products.size()) == 0) {
+//            products.add(product);
+//            totalOrderCost = getTotalOrderCost();
+//        } else {
+//            addProductToCart();
+//            totalOrderCost = getTotalOrderCost();
+//        }
 
 //        return new Product()
-    }
-}
+
 
--- src/test/java/CartTests/CartTests.java
+++ src/test/java/CartTests/CartTests.java
@@ -10,22 +10,22 @@ import start.Pages;
 public class CartTests extends Pages {
     private static Logger logger = LoggerFactory.getLogger(CartTests.class);
 
-    //        @RepeatedTest(10)
-    @Test
-    @DisplayName("Verify number of products in categories display")
-    public void cartPopup() {
-        headerPage.enterCategoryByName("ART");
-        productsPage.openBestPoster();
-        singleProductPage.setProductQuantity("3");
-        singleProductPage.addProductToCart();
-
-        for (Product product : cart) {
-
-        }
-
-        softly.assertThat(productDialogPage.getProductName()).isEqualTo(product.get);
-        softly.assertThat(productDialogPage.getProductPrice()).isEqualTo();
-        softly.assertThat(basePage.productsCountInString(productDialogPage.getProductCountString())).isEqualTo();
-    }
+//    //        @RepeatedTest(10)
+//    @Test
+//    @DisplayName("Verify number of products in categories display")
+//    public void cartPopup() {
+//        headerPage.enterCategoryByName("ART");
+//        productsPage.openBestPoster();
+//        singleProductPage.setProductQuantity("3");
+//        singleProductPage.addProductToCart();
+//
+//        for (Product product : cart) {
+//
+//        }
+//
+//        softly.assertThat(productDialogPage.getProductName()).isEqualTo(product.get);
+//        softly.assertThat(productDialogPage.getProductPrice()).isEqualTo();
+//        softly.assertThat(basePage.productsCountInString(productDialogPage.getProductCountString())).isEqualTo();
+//    }
 
 }
