8c8f9fce5356700067a9a86d57d69ba6bc90d54a
Mateusz <mateusz.bukowski.ext@iceservices.com>
2022-11-26 22:56:17 +0100

    update po review #5

--- src/main/java/models/Cart.java
+++ src/main/java/models/Cart.java
@@ -31,29 +31,19 @@ public class Cart {
         logger.info("Current total order cost: " + totalOrderCost);
         totalOrderCost = totalOrderCost + (newProduct.getProductQuantity() * getPrice(newProduct.getProductPrice()));
         logger.info("New total order cost: " + totalOrderCost);
-        if (isProductAlreadyInCart(products, newProduct)) {
-            for (Product product : products) {
-                if (product.getProductName().equals(newProduct.getProductName())) {
-                    logger.info("Old quantity:" + product.getProductQuantity() + " ___ " + "New quantity: " + newProduct.getProductQuantity());
-                    product.setQuantity(product.getProductQuantity() + newProduct.getProductQuantity());
-                    logger.info("New quantity: " + product.getProductQuantity());
-                    logger.info("Increased quantity in cart, cart size: " + products.size());
-                    return;
-                }
+        for (Product product : products) {
+            if (product.getProductName().equals(newProduct.getProductName())) {
+                logger.info("Old quantity:" + product.getProductQuantity() + " ___ " + "New quantity: " + newProduct.getProductQuantity());
+                product.setQuantity(product.getProductQuantity() + newProduct.getProductQuantity());
+                logger.info("New quantity: " + product.getProductQuantity());
+                logger.info("Increased quantity in cart, cart size: " + products.size());
+                return;
             }
         }
         products.add(newProduct);
         logger.info("Added product to cart, cart size: " + products.size());
     }
 
-    public boolean isProductAlreadyInCart(List<Product> list, Product product) {
-        for (Product prod : list) {
-            if (prod.getProductName().equals(product.getProductName())) {
-                return true;
-            }
-        }
-        return false;
-    }
     public Double getPrice(String price) {
         return Double.parseDouble(price.replace(System.getProperty("currency"), ""));
     }
